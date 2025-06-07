9b83edd6015bb7e5ee754ac5b931da06229b7ed8
hahahah12345678 <rhysmvanf@gmail.com>
2025-06-07 15:31:39 +0000

    Update all changes

--- download steam.py and thankyou.html/setup.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name="SteamApp",
-    version="1.0",
-    description="Steam app with thankyou.html",
-    executables=[Executable("steam.py")],
-    include_files=["thankyou.html"],
-)
